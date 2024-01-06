9c6293a88a0b8bcb2b8bdc7d0057dc7048160a7b
Maxim <msirotkin95@gmail.com>
2024-01-06 12:47:42 +0300

    test2

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -2,10 +2,6 @@ package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
