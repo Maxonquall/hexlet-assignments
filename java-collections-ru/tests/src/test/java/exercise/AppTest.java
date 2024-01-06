4fcf3b5d2e2ee876d64e1b9c92a3c89fafc0e3bd
Maxim <msirotkin95@gmail.com>
2024-01-06 11:58:23 +0300

    test2

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -12,22 +12,9 @@ class AppTest {
 
     @Test
     void testTake() {
-      //  var list = List.of(1, 2, 3, 4, 5, 6);
-      //  var expected = List.of(1, 2, 3);
-        var list = new ArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        var expected = new ArrayList<Integer>();
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-
+        var list = List.of(1, 5, 3, 4, 5, 6);
+        var expected = List.of(1, 5, 3);
         var actual = App.take(list, 3);
         assertThat(expected).isEqualTo(actual);
-      //  Assertions.assertEquals(expected, list);
     }
 }
