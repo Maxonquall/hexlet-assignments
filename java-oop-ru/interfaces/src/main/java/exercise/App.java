d70d22a632bb55ee5de295362f7959c958c055a4
@hexlet/cli <support@hexlet.io>
2024-03-03 18:38:20 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
-    public static List<String> buildApartmentsList(List<Home> homes, int n) {
-        return homes.stream()
-                .sorted()
+    public static List<String> buildApartmentsList(List<Home> apartments, int n) {
+        return apartments.stream()
+                .sorted(Home::compareTo)
                 .limit(n)
                 .map(Object::toString)
                 .collect(Collectors.toList());
     }
+
 }
--- java-oop-ru/interfaces/src/main/java/exercise/Home.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Home.java
@@ -1,6 +1,6 @@
 package exercise;
 
-public interface Home extends Comparable<Home> {
+public interface Home /*extends Comparable<Home>*/ {
     double getArea();
     int compareTo(Home another);
 }
