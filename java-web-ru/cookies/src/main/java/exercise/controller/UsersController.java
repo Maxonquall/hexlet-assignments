d70a0f3d6abdaf52535cfea11d211f316670a67c
@hexlet/cli <support@hexlet.io>
2024-04-27 20:54:56 +0300

    submit java-web-ru/cookies

--- java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -20,29 +20,32 @@ public class UsersController {
     }
 
     // BEGIN
-    public static void create(Context ctx) {
-        String firstName = ctx.formParam("firstName");
-        String lastName = ctx.formParam("lastName");
-        String email = ctx.formParam("email");
-        String password = ctx.formParam("password");
-        String encryptPassword = Security.encrypt(password);
-        String token = Security.generateToken();
-        User user = new User(firstName, lastName, email, encryptPassword, token);
+    public static void create(Context ctx) throws Exception {
+        var firstName = StringUtils.capitalize(ctx.formParam("firstName"));
+        var lastName = StringUtils.capitalize(ctx.formParam("lastName"));
+        var email = ctx.formParam("email").trim().toLowerCase();
+        var password = ctx.formParam("password");
+        var encryptedPassword = Security.encrypt(password);
+        var token = Security.generateToken();
+
+        var user = new User(firstName, lastName, email, encryptedPassword, token);
         UserRepository.save(user);
+
         ctx.cookie("token", token);
-        ctx.redirect("/users/" + String.valueOf(user.getId()));
+        ctx.redirect(NamedRoutes.userPath(user.getId()));
     }
 
-    public static void show(Context ctx) {
-        Long id = ctx.pathParamAsClass("id", Long.class).get();
-        User user = UserRepository.find(id)
-                .orElseThrow(() -> new NotFoundResponse("User not found"));
-        String token = ctx.cookie("token");
-        if (user.getToken().equals(token)) {
-            ctx.render("users/show.jte", Collections.singletonMap("user", user));
-        } else {
+    public static void show(Context ctx) throws Exception {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var token = ctx.cookie("token") == null ? null : ctx.cookie("token");
+        var user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        if (user == null || !user.getToken().equals(token)) {
             ctx.redirect(NamedRoutes.buildUserPath());
+            return;
         }
+        var page = new UserPage(user);
+        ctx.render("users/show.jte", model("page", page));
     }
     // END
 }
