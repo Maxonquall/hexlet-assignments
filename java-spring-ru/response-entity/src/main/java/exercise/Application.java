9e4affbb0e845810c84a456804fcaa2cdb2b1bcf
@hexlet/cli <support@hexlet.io>
2024-06-01 12:26:33 +0300

    submit java-spring-ru/response-entity

---
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -0,0 +1,79 @@
+package exercise;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts") // Список страниц
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "10") Integer limit,
+                                            @RequestParam(defaultValue = "1") Integer page) {
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(posts.size()))
+                .body(result);
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> create(@RequestBody Post page) {
+        posts.add(page);
+        return ResponseEntity.status(HttpStatus.CREATED).body(page);
+    }
+
+    @GetMapping("/posts/{id}") // Вывод страницы
+    public ResponseEntity<Post> show(@PathVariable String id) {
+        var page = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        return ResponseEntity.of(page);
+    }
+
+    @PutMapping("/posts/{id}") // Обновление страницы
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post data) {
+        var maybePage = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        var status = HttpStatus.NO_CONTENT;
+        if (maybePage.isPresent()) {
+            var page = maybePage.get();
+            page.setId(data.getId());
+            page.setTitle(data.getTitle());
+            page.setBody(data.getBody());
+            status = HttpStatus.OK;
+        }
+        return ResponseEntity.status(status).body(data);
+    }
+
+    // END
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+}
